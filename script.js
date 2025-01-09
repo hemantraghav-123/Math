73811e333a2055e8c77ce5378ef0f75319fc0c50
hemantraghav-123 <hemant.23b0131263@abes.ac.in>
2025-01-09 12:17:31 +0530

    Math

---
+++ script.js
@@ -0,0 +1,19 @@
+// Math.round(4.7)
+// Math.round(4.9)
+// Math.round(4.2)
+// Math.round(-4.7)
+// console.log()
+
+// Math.ceil(4.7)
+// Math.ceil(4.9)
+// Math.ceil(4.2)
+// Math.ceil(-4.7)
+
+
+// Math.floor(4.7)
+// Math.floor(4.9)
+// Math.floor(4.2)
+// Math.floor(-4.7)
+
+
+Math.min(0,150,30,20,-8,-200)
